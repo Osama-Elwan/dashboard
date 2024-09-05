c4bc6ab12bb74d25e717ccf2ceaf4df516c69afc
Osama-Elwan <osama.elwan01@gmail.com>
2024-09-06 02:17:47 +0300

    finish master layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('admin')->group(function(){
+    Route::get('/',HomeController::class)->name('users.home');
+    Route::controller(StudentController::class)->group(function(){
+        Route::get('/users','index')->name('admin.users.index');
+    });
 });
